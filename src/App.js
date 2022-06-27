4f7f4cb239b60c85d20e9cc207b6f67cd06cb565
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-06-27 19:30:00 +0200

    Edit api fetch and add catch statment but faild

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import {useState, useEffect} from 'react';
 
 export default function App() {
-  const [cameraData, setCameraData] = useState([]);
+  const [participantData, setParticipantData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(
-      `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/project_greenlight/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson`
+      'https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/project_greenlight/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
     )
-      .then(response => response.json())
-      .then(fetchData => {
-        setCameraData(fetchData.features);
+      .then(response => {
+        if (!response.ok) {
+          throw Error('We are sorry! Web-API is currently offline.');
+        }
+        return response.json();
       })
-      .catch(error => {
-        setError('Sorry! Data is currently not available.');
+      .then(data => {
+        setParticipantData(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <main className="App">
+      <h1>Camera Surveillance</h1>
+      {error && <div>{error}</div>}
+      {loading && <div>Is Loading...</div>}
       <ul>
-        {cameraData.map(features => (
+        {participantData.map(features => (
           <li key={features}>
+            {features.properties.ObjectId}
             {features.properties.business_name}
+            {features.properties.business_type}
             {features.properties.address}
             {features.properties.live_date}
+            {features.properties.precinct}
           </li>
         ))}
       </ul>
