d09571b85e9f81190bd90f3cd22e7432305d5628
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-06-27 16:33:47 +0200

    Fetch and Render API in app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
+import {useState, useEffect} from 'react';
+
 export default function App() {
-  return <div>App goes here</div>;
+  const [cameraData, setCameraData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/project_greenlight/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson`
+    )
+      .then(response => response.json())
+      .then(fetchData => {
+        setCameraData(fetchData.features);
+      })
+      .catch(error => {
+        setError('Sorry! Data is currently not available.');
+      });
+  }, []);
+
+  return (
+    <main className="App">
+      <ul>
+        {cameraData.map(features => (
+          <li key={features}>
+            {features.properties.business_name}
+            {features.properties.address}
+            {features.properties.live_date}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
