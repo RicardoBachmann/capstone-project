03568bc66e074a04eea69e5da0c76b7937475745
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-07-12 19:02:51 +0200

    Edit annotations from code review

--- src/components/Participant/ParticipantCard.js
+++ src/components/Participant/ParticipantCard.js
@@ -2,34 +2,53 @@ import styled from 'styled-components';
 
 export default function ParticipantCard({name, businessType, address, liveDate, precinct, handleFlyTo}) {
   return (
-    <ParticipantButton onClick={handleFlyTo}>
+    <ParticipantContainer>
       <h2>{name}</h2>
-      <p>Business Type: {businessType}</p>
-      <p>Address: {address}</p>
-      <p>Since: {liveDate}</p>
-      <p>Precint: {precinct}</p>
-    </ParticipantButton>
+      <ul>
+        <li>Business Type: {businessType}</li>
+        <li>Address: {address}</li>
+        <li>Since: {liveDate}</li>
+        <li>Precint: {precinct}</li>
+      </ul>
+      <button onClick={handleFlyTo}>INVESTIGATE</button>
+    </ParticipantContainer>
   );
 }
 
-const ParticipantButton = styled.button`
+const ParticipantContainer = styled.section`
+  display: grid;
   width: 100%;
-  font-size: 0.7rem;
-  line-height: 0.3rem;
+  font-size: 0.6rem;
+  line-height: 0.8rem;
   color: white;
   background-color: black;
   border: solid 1px lightgrey;
   text-align: left;
-  padding-left: 0.6rem;
 
-  &:focus {
+  button {
     background-color: black;
+    font-size: 0.8rem;
     color: lime;
     border: solid 1px red;
+    padding: 0.3rem;
+    margin: 0.6rem;
+
+    &:focus {
+      background-color: red;
+    }
   }
 
   h2 {
-    font-size: 1.0rem;
+    text-align: center;
+    font-size: 1rem;
+    line-height: 0.1rem;
     font-weight: 400;
+  }
+
+  ul {
+    text-align: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
   }
 `;
